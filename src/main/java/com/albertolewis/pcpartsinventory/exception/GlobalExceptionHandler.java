572778758364247a5348d0a6d756a70d46bb877f
Alberto Lewis <AlbertoLewis1567@gmail.com>
2026-09-19 13:45:17 -0500

    fix: Handle restock part not being found or invalid

--- src/main/java/com/albertolewis/pcpartsinventory/exception/GlobalExceptionHandler.java
+++ src/main/java/com/albertolewis/pcpartsinventory/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.albertolewis.pcpartsinventory.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,6 +17,15 @@ public class GlobalExceptionHandler {
         error.put("message", ex.getMessage());
         error.put("status", 400);
         return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(PcPartNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handlePcPartNotFoundException(PcPartNotFoundException ex){
+        Map<String, Object> error = new HashMap<>();
+        error.put("error", "Not Found");
+        error.put("message", ex.getMessage());
+        error.put("status", HttpStatus.NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
 }
